493de356953549fc110fc5ebea726b3d08cb3df6
DevendraBhatta <devendrabhatta63@gmail.com>
2025-12-29 13:53:53 +0545

    small-changes

--- 02-generating-html.php
+++ 02-generating-html.php
@@ -1,20 +1,30 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Document</title>
     <style>
-        .body-1{
-        background-color:red;
+        .body-1 {
+            background-color: red;
         }
-        </style>
+
+        .body-2 {
+            background-color: green;
+        }
+        .body-3 {
+            background-color: aqua;
+        }
+        .body-4 {
+            background-color: grey;
+        }
+ 
+    </style>
 </head>
-<body>
-<h1>Hello </h1>
- <!--  inner php outer html-->
-<h1> <?php echo 'Hello from PHP';?></h1>
-<!-- inner html outer php-->
-<?php echo '<h1> Hello from PHP</h1>'; ?>
+
+<body class="body-<?php echo rand(1,4); ?>">
+    <?php echo '<h1> Hello from PHP</h1>'; ?>
+
 </body>
 </html>
